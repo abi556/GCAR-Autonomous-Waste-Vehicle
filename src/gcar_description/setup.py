a175f9d39d168325175bb5c1748bc921f7300c41
Abiy Hailu <abiymit@outlook.com>
2026-01-28 22:12:07 +0300

    last version before submission

--- src/gcar_description/setup.py
+++ src/gcar_description/setup.py
@@ -13,6 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include config files (controllers, params, etc.)
+        (os.path.join('share', package_name, 'config'),
+            glob(os.path.join('config', '*.yaml'))),
         # Include launch files
         (os.path.join('share', package_name, 'launch'),
             glob(os.path.join('launch', '*.launch.py'))),
